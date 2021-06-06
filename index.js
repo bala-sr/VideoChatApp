e85c51c0939dde2def553919698a55d8dee7370c
Balasubramaniam <balasr97@gmail.com>
2021-06-06 18:43:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const Express = require("express");
+const cors = require("cors");
+const http = require('http');
+// import {Server} from "socket.io;"
+const app = Express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3200/",
+        methods: ["GET", "POST"]
+    }
+});
+const port = 3200;
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+
+    //Disconnect call
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("Call Ended");
+    })
+
+    //Calling user
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", {signal: data.signalData, from: data.from, name: data.name});
+    })
+
+    //Answering call
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    })
+});
+
+// app.get("/", (req, res) => res.send("hello"));
+
+server.listen(port, () => console.log("Server running in port ", port));
